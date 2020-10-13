b68365e7f1a7d618654ad144c6d69f53d46280d9
Shawn Burke <sburke@uber.com>
2020-10-13 10:27:14 -0700

    Brew work and readme

---
+++ README.md
@@ -0,0 +1,38 @@
+# Usage
+
+## Basic system setup
+For systems that have nothing, e.g. a Debian container run:
+
+```
+. dotfiles/tools/prereq.sh
+```
+
+This will install sudo, git, python.
+
+
+## Standard system setup
+
+Gets the right tools and packages in place:
+
+```
+. dotfiles/tools/init.sh
+```
+
+Idempotent so should be fine to run whenever.
+
+## Do the things!
+
+Once the above is set, run the install to set up the system.
+
+```
+. dotfiles/install
+```
+
+And that should set up all the links.  To get everything working, it typically is best to exit the current session and log back in.  A cheat is to just run `zsh`.
+
+Note Powerline will need the Powerline (PL) fonts for your terminal of choice.
+
+See more:
+
+* [General linux powerline fonts](https://github.com/powerline/fonts)
+* [VSCode / Windows Terminal](https://docs.microsoft.com/en-us/windows/terminal/tutorials/powerline-setup).  Install the fonts then use `Cascadia Code PL` (or mono pl)
--- tools/init.sh
+++ tools/init.sh
@@ -1,11 +1,6 @@
 
-function install_linux_binary {
-
-    package=$2
-
-    package=${package:-$1}
-
-    if ! which $1 >/dev/null 
+function install_apt_package {
+    if ! dpkg -l $1 2&>1 >/dev/null
     then
         echo "Installing $1..."
         sudo apt-get install -yq $package >/dev/null
@@ -16,55 +11,57 @@ function install_linux_binary {
 }
 
 function install_linux {
-
-    echo "Installing fonts-powerline..."
-    sudo apt-get install -yq fonts-powerline >/dev/null
-    install_linux_binary curl
-    install_linux_binary wget
-    install_linux_binary highlight
-    install_linux_binary jq
-    install_linux_binary zsh
-    install_linux_binary vim
-    install_linux_binary tmux
-    install_linux_binary nc netcat
-    install_linux_binary direnv
-    install_linux_binary autojump
-
+    sudo apt-get install -yq \
+        curl wget netcat \
+        highlight \
+        jq \
+        zsh \
+        direnv autojump \
+        vim tmux
     # install oh-my-zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 
 }
 
+function install_brew {
+    if ! brew list $1 2&>1 > /dev/null
+    then
+        echo "Installing $1"
+        brew install $1
+    else
+        echo "$1 already installed"
+    fi
+}
+
 
 function install_osx {
     # brew
     # tools above
-    if ! which brew </dev/null
+    if ! which brew >/dev/null
     then 
         echo "Please install brew, then run again."
-        exit 1
+        return
     fi
 
-    brew install curl
-    brew install wget
-    brew install highlight
-    brew install jq
-    brew install autojump
+    install_brew wget
+    install_brew highlight
+    install_brew jq
+    install_brew autojump
+    install_brew direnv
+    install_brew tmux
+    install_brew autojump
 }
 
-
+ 
 os=$(uname -s)
-
-
-
 
 case $os in
 Linux)
     install_linux
     ;;
 Darwin)
-    install_darwin
+    install_osx
     ;;
 *)
     echo "Unknown OS (${os})"
