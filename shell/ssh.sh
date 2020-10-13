09771b92a27507cadc01f2c37f8b69464bb81315
Shawn Burke <shawn@shawnburke.com>
2020-10-13 15:29:22 -0700

    Fix ssh-add

--- shell/ssh.sh
+++ shell/ssh.sh
@@ -2,6 +2,7 @@ eval $(ssh-agent) >/dev/null
 
 if [ -d ~/.ssh ]
 then 
-   # Get all keys that start with id_ in .ssh dir, add to ssh-add
-   find . -name "id_*" -maxdepth 1 -type f ! -name "*.*"  | xargs -I S ssh-add S >/dev/null;
+   # Get all keys that start with id_ in .ssh dir, add to ssh-add. ssh-add outputs to stderr, so we want to suppress
+   # that or zsh/powershell will get mad.  Standard output is like "Identity added..."
+   find ~/.ssh -maxdepth 1 -name "id_*" -type f ! -name "*.*" | xargs -I S ssh-add S 2&>1 | grep -vE "^Identity"
 fi
